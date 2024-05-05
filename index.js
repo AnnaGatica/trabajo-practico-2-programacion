93d5bb982932df25f9453704b41879ac3ab5256f
AnnaGatica <166068871+AnnaGatica@users.noreply.github.com>
2024-05-05 02:32:30 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,122 @@
+//1
+lefnumerouno= number(prompt)(("Escribe un numero"))
+lefnumerodos= number(prompt)(("Escribe otro numero"))
+
+if (numerouno < numerodos){
+    alert(numerouno + "Es mayor a"+ numerodos)
+{else}
+    alert(numerodos + "Es mayor a" + numerouno)
+}
+
+
+//2 
+letNumero= Number (prompt) (("Escribe un numero"))
+if ((Numero>-1)  || (Numeero<-0)) {
+    alert(Numero + "El numero es positivo")  || alert("El numero es negativo")
+}else{
+    alert("El numero es cero")
+}
+
+//3 
+letNumero3= Number (prompt)(("Escribe un numero cualquiera"))
+letResultado= Numero3/2 
+if (resultado == 0) {
+    alert("El numero es par")
+} else { 
+    alert("El numero es impar")
+} 
+
+//4 
+let EdadUsuario= prompt("Ingresa tu edad")
+if (EdadUsuario >= 18) {
+    alert("Sos mayor de edad")
+}else{
+    alert("Sos menor de edad")
+}
+
+//5
+letdia= number (prompt)("Escribe un numero del 1 al 7")
+if (dia==1){
+    alert("Ese dia es Lunes")
+}else if (dia==2){
+    alert("Ese dia es Martes")
+}
+else if (dia==3){
+    alert("Ese dia es Miercoles")
+}else if ("dia==4") {
+    alert("Ese dia es Jueves")
+}else if (dia==5){
+    alert("Ese dia es Viernes")
+}else if (dia==6){
+    alert("Ese dia es Sabado")
+}else if (dia==7){
+    alert("Ese dia es Domingo")
+}
+
+//6
+letnumero= prompt("Ingresa un numero")
+letresultado1=numero4/3
+letresultado2=numero4/5
+if ((resultado1==0)&& (resultado2==0)){
+    alert("El numero que has ingresado es divisible por 3 y por 5")
+}else{
+    alert("El numero que has ingresado no es divisible por 3 y por 5")
+}
+
+//7 
+letnumero=prompt("Ingresa un numero")
+if ((numero>=1)|| (numero<=0) && (numero<100)){
+    alert("El numero ingresado es par y menor a 100")
+}else{
+    alert("El numero ingresado no es par y mayor a 100")
+}
+
+//8 
+letEdad= prompt("Ingresa tu edad")
+letGenero=prompt("Ingresa tu genero")
+if ((Edad==65) && (Genero == "Varon")){
+    alert("Ya estas en edad para jubilarte")
+}else{
+    alert("Aun no estas en edad para jubilarte")
+}if ((Edad==60)&& (Genero=="mujer")){
+    alert("Ya estas en edad para jubilarte")
+}else{
+    alert("Aun no estas en edad para jubilarte")
+}
+
+//9
+letNumero6= Number(Prompt)(("Ingrese un numero"))
+letNumero7=Number(Pormpt)(("Ingresa un numero"))
+letNumero8=Number(prompt)(("Ingresa un numero"))
+if(Numero6==Numero7){
+    alert(Numero6 + "Y" + Numero7 + "Son iguales")
+}else if (Numero6==Numero8){
+    alert(Numero6 + "Y" + Numero8 + "Son iguales")
+}else if (Numero7 ==Numero8){
+    alert(Numero7 + "Y" + Numero8 + "Son iguales")
+}
+
+//10
+letnumero9=Number(prompt)("Ingresar un numero")
+letresultado3=numero9/4
+letresultado4=numero9/6
+letresultado5=numero9/9
+letresultado6=numero9/12
+
+if ((resultado3==0)&& (resultado4==0) && (resultado5==0)&& (resultado6==0)) {
+    alert("El numero ingresado es divisible por 4, 6, 9 y 12")
+}else{
+    alert("El numero ingresado no es divisible por 4, 6, 9 y 12")
+}
+
+//11
+letedad1=Number(prompt)(("Ingresa una edad"))
+letedad2=Number(prompt)(("Ingresa una edad"))
+letedad3=Number(Prompt)(("ingresa una edad"))
+if (edad1>=18){
+    alert(edad1 + "Eres mayor de edad")
+}else if (edad2>=18){
+    alert(edad2 + "Eres mayor de edad")
+}else if (edad3>=18){
+    alert(edad3 + "Eres mayor de edad")
+}
